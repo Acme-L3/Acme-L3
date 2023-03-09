eecafc9241a899e7394c954586896e4f649643fd
Antonio23337 <antoniobarea233@gmail.com>
2023-03-09 09:53:52 +0100

    Añadido algunos cambios mas

--- src/main/java/acme/entitites/tutorial/Tutorial.java
+++ src/main/java/acme/entitites/tutorial/Tutorial.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
@@ -15,6 +17,7 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 import acme.framework.data.AbstractEntity;
+import acme.roles.Assistant;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,7 +30,7 @@ public class Tutorial extends AbstractEntity {
 
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
 	protected String			code;
 
 	@NotBlank
@@ -52,4 +55,10 @@ public class Tutorial extends AbstractEntity {
 	@NotNull
 	protected Date				endDate;
 
+	//Relations -----------------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne()
+	protected Assistant			assistant;
 }
