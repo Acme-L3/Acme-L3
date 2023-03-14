b45f13f18773f236650533d8c77cb07f60b97b9f
Antonio23337 <antoniobarea233@gmail.com>
2023-03-14 21:30:15 +0100

    Añadido Assistant1 y Assistant2 a la base de datos y corregido diversos errores

--- src/main/java/acme/roles/Assistant.java
+++ src/main/java/acme/roles/Assistant.java
@@ -1,16 +1,12 @@
 
 package acme.roles;
 
-import java.util.Collection;
-
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.entitites.tutorial.Tutorial;
 import acme.framework.data.AbstractRole;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,22 +16,19 @@ import lombok.Setter;
 @Setter
 public class Assistant extends AbstractRole {
 
-	protected static final long		serialVersionUID	= 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	@NotBlank
 	@Length(max = 75)
-	protected String				supervisor;
+	protected String			supervisor;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				curriculum;
+	protected String			curriculum;
 
 	@URL
-	protected String				link;
+	protected String			link;
 
 	//Relations -----------------------------------------------------------------
-
-	@OneToMany(mappedBy = "assistant")
-	protected Collection<Tutorial>	tutorials;
 
 }
