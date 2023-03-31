071a5ca1dd7d26248f2262030a9e4e1594914ea4
Antonio23337 <antoniobarea233@gmail.com>
2023-03-31 17:00:40 +0200

    Añadida la tarea 22 del documento grupal y corrección de errores

---
+++ src/main/java/acme/features/authenticated/bulletin/AuthenticatedBulletinRepository.java
@@ -0,0 +1,22 @@
+
+package acme.features.authenticated.bulletin;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entitites.bulletins.Bulletin;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AuthenticatedBulletinRepository extends AbstractRepository {
+
+	@Query("select b from Bulletin b where b.id = :id")
+	Bulletin findOneBulletinById(int id);
+
+	@Query("select b from Bulletin b where b.moment >= :deadline")
+	Collection<Bulletin> findRecentBulletins(Date deadline);
+
+}
