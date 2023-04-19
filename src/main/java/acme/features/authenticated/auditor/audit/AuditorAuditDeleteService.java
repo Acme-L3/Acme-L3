73a1b46603bbaf50815039f5f326526a6199fbce
elguille2 <guillermo2pacheco@gmail.com>
2023-04-19 11:52:29 +0200

    Operation by auditors on audits

--- src/main/java/acme/features/authenticated/auditor/audit/AuditorAuditDeleteService.java
+++ src/main/java/acme/features/authenticated/auditor/audit/AuditorAuditDeleteService.java
@@ -1,6 +1,98 @@
+
 package acme.features.authenticated.auditor.audit;
 
+import java.util.Collection;
 
-public class AuditorAuditDeleteService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entitites.audits.Audit;
+import acme.entitites.audits.AuditingRecord;
+import acme.entitites.course.Course;
+import acme.framework.components.jsp.SelectChoices;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+import acme.roles.Auditor;
+
+@Service
+public class AuditorAuditDeleteService extends AbstractService<Auditor, Audit> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuditorAuditRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void check() {
+		final boolean status = super.getRequest().hasData("id", int.class);
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+		int auditId;
+		Audit audit;
+		Auditor auditor;
+
+		auditId = super.getRequest().getData("id", int.class);
+		audit = this.repository.findAuditById(auditId);
+		auditor = audit == null ? null : audit.getAuditor();
+		status = audit != null && audit.isDraftMode() && super.getRequest().getPrincipal().hasRole(auditor);
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		final int id = super.getRequest().getData("id", int.class);
+		final Audit object = this.repository.findAuditById(id);
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Audit object) {
+		assert object != null;
+		int courseId;
+		Course course;
+
+		courseId = super.getRequest().getData("id", int.class);
+		course = this.repository.findCourseById(courseId);
+		super.bind(object, "code", "conclusion", "strongPoints", "weakPoints", "draftMode");
+		object.setCourse(course);
+	}
+
+	@Override
+	public void validate(final Audit object) {
+		assert object != null;
+	}
+
+	@Override
+	public void perform(final Audit object) {
+		assert object != null;
+		final Collection<AuditingRecord> auditingRecords;
+
+		auditingRecords = this.repository.findAuditingRecordsByAuditId(object.getId());
+		this.repository.deleteAll(auditingRecords);
+		this.repository.delete(object);
+	}
+
+	@Override
+	public void unbind(final Audit object) {
+		assert object != null;
+		Collection<Course> courses;
+		final SelectChoices coursesChoices;
+		final Tuple tuple;
+
+		courses = this.repository.findAllCourses();
+		coursesChoices = SelectChoices.from(courses, "title", object.getCourse());
+		tuple = super.unbind(object, "code", "conclusion", "strongPoints", "weakPoints", "draftMode");
+		tuple.put("course", coursesChoices.getSelected().getKey());
+		tuple.put("courses", coursesChoices);
+
+		super.getResponse().setData(tuple);
+	}
 
 }
--- src/main/java/acme/features/authenticated/auditor/audit/AuditorAuditRepository.java
+++ src/main/java/acme/features/authenticated/auditor/audit/AuditorAuditRepository.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import acme.entitites.audits.Audit;
+import acme.entitites.audits.AuditingRecord;
 import acme.entitites.course.Course;
 import acme.framework.repositories.AbstractRepository;
 import acme.roles.Auditor;
@@ -21,5 +23,14 @@ public interface AuditorAuditRepository extends AbstractRepository {
 
 	@Query("select c from Course c")
 	Collection<Course> findAllCourses();
+
+	@Query("select a from Audit a where a.id = :id")
+	Audit findAuditById(int id);
+
+	@Query("select ar from AuditingRecord ar where ar.audit.id = :id")
+	Collection<AuditingRecord> findAuditingRecordsByAuditId(int id);
+
+	@Query("select a from Audit a where a.auditor.id = :id")
+	Collection<Audit> findAuditsByAuditorId(int id);
 
 }
