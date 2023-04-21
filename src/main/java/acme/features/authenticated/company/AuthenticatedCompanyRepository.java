219f2b02fb5e9164398e029f3e62a2870469bc22
Javier Rodríguez Cordero <javrodcor@alum.us.es>
2023-04-21 12:04:42 +0200

    Company Update done

--- src/main/java/acme/features/authenticated/company/AuthenticatedCompanyRepository.java
+++ src/main/java/acme/features/authenticated/company/AuthenticatedCompanyRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.Query;
 
 import acme.framework.components.accounts.UserAccount;
 import acme.framework.repositories.AbstractRepository;
+import acme.roles.Company;
 
 public interface AuthenticatedCompanyRepository extends AbstractRepository {
 
 	@Query("SELECT u FROM UserAccount u WHERE u.id = :userAccountId")
 	UserAccount findOneUserAccountById(int userAccountId);
+
+	@Query("SELECT c FROM Company c WHERE c.userAccount.id = :userAccountId")
+	Company findOneCompanyByUserAccountId(int userAccountId);
 
 }
