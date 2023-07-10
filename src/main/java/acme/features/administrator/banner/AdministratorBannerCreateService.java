dab31003c09d514a05d054b05632b50a3fcfb0f0
Antonio23337 <antoniobarea233@gmail.com>
2023-07-10 19:01:47 +0200

    pequeñas correcciones

--- src/main/java/acme/features/administrator/banner/AdministratorBannerCreateService.java
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerCreateService.java
@@ -2,6 +2,7 @@
 package acme.features.administrator.banner;
 
 import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +56,33 @@ public class AdministratorBannerCreateService extends AbstractService<Administra
 	public void validate(final Banner object) {
 		assert object != null;
 		final Errors errors = super.getBuffer().getErrors();
+
+		if (!super.getBuffer().getErrors().hasErrors("startDate")) {
+
+			final Calendar calendar1 = Calendar.getInstance();
+			calendar1.set(Calendar.YEAR, 2000);
+			calendar1.set(Calendar.MONTH, Calendar.JANUARY);
+			calendar1.set(Calendar.DAY_OF_MONTH, 1);
+			calendar1.set(Calendar.HOUR_OF_DAY, 00);
+			calendar1.set(Calendar.MINUTE, 00);
+			calendar1.set(Calendar.SECOND, 00);
+			calendar1.set(Calendar.MILLISECOND, 0);
+			final Date iniDate = calendar1.getTime();
+			final Calendar calendar = Calendar.getInstance();
+			calendar.set(Calendar.YEAR, 2100);
+			calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+			calendar.set(Calendar.DAY_OF_MONTH, 31);
+			calendar.set(Calendar.HOUR_OF_DAY, 23);
+			calendar.set(Calendar.MINUTE, 59);
+			calendar.set(Calendar.SECOND, 00);
+			calendar.set(Calendar.MILLISECOND, 0);
+			final Date limitDate = calendar.getTime();
+			boolean date1;
+			boolean date2;
+			date1 = MomentHelper.isAfterOrEqual(object.getStartDate(), iniDate);
+			date2 = MomentHelper.isBeforeOrEqual(object.getEndDate(), limitDate);
+			super.state(date1 && date2, "startDate", "administrator.baner.form.error.limit.date");
+		}
 
 		if (!super.getBuffer().getErrors().hasErrors("startDate")) {
 			final boolean b = object.getStartDate().after(MomentHelper.getCurrentMoment());
